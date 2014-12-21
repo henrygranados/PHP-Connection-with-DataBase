810aa02ef35f9f265baeccfc15c1eaf163c93c1a
henry <henrygranados03@gmail.com>
2014-12-21 03:54:58 -0700

    added index.php file

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+	/**
+	 * Author: Henry Granados
+	 * Variables that will store localhost, user and password values.
+	 */
+	$mysql_host = 'localhost';
+	$mysql_user = 'root';
+	$mysql_pass = 'compu123';
+	/**
+	 *variable that stores the table that we are using from our DataBase.
+	 */
+	$mysql_db = 'personas';
+	
+	/**
+	 * connection with MySql DataBase using localhost, user and password.
+	 */
+	@mysql_connect($mysql_host , $mysql_user , $mysql_pass) or die('cannot connect');
+	@mysql_select_db($mysql_db) or die('No se pudo conectar');
+	
+	echo 'Connected to Database'.'<br/><br/>';
+?>
